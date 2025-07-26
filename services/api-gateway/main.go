5745ef46666eca78e5a7d830945195b84b42391f
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-07-26 17:58:50 +0700

    feat(gateway): add websocket handlers for drivers and riders

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -22,6 +22,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
+	mux.HandleFunc("/ws/drivers", handleDriversWebSocket)
+	mux.HandleFunc("/ws/riders", handleRidersWebSocket)
 
 	server := &http.Server{
 		Addr:    httpAddr,
---
+++ services/api-gateway/ws.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"ride-sharing/shared/contracts"
+	"ride-sharing/shared/util"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade failed: %v", err)
+		return
+	}
+
+	defer conn.Close()
+
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		log.Println("No user id provided")
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
+}
+
+func handleDriversWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("WebSocket upgrade failed: %v", err)
+		return
+	}
+
+	defer conn.Close()
+
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		log.Println("No user id provided")
+		return
+	}
+
+	packageSlug := r.URL.Query().Get("packageSlug")
+	if packageSlug == "" {
+		log.Println("No user id provided")
+		return
+	}
+
+	type Driver struct {
+		ID             string `json:"id"`
+		Name           string `json:"name"`
+		ProfilePicture string `json:"profilePicture"`
+		CarPlate       string `json:"carPlate"`
+		packageSlug    string `json:"packageSlug"`
+	}
+
+	wsMessage := contracts.WSMessage{
+		Type: "driver.cmd.register",
+		Data: Driver{
+			ID:             userID,
+			Name:           "John Doe",
+			ProfilePicture: util.GetRandomAvatar(1),
+			CarPlate:       "ABC123",
+			packageSlug:    packageSlug,
+		},
+	}
+
+	if err := conn.WriteJSON(wsMessage); err != nil {
+		log.Printf("Error sending message: %v", err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
+}
